d97a77ed5f080df7cdf7df76ce2673ca2cbcdc1a
Aman Kumar Gupta <amang5840@gmail.com>
2024-07-16 00:59:00 +0530

    Find single number

--- ArraysEasy.java
+++ ArraysEasy.java
@@ -200,6 +200,15 @@ public class ArraysEasy {
         return count;
     }
 
+    static int singleNumber(int[] arr) {
+        int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            num ^= arr[i];
+        }
+
+        return num;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 4, 7, 7, 5 };
         int[] arr1 = { 28078, 19451, 935, 28892, 2242, 3570, 5480, 231 };
