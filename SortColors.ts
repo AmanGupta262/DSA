7ddb59de11b3aa8624de973c904e0b1f09b5b148
Aman Kumar Gupta <amang5840@gmail.com>
2024-07-18 00:11:33 +0530

    Sort colors brute force

--- SortColors.ts
+++ SortColors.ts
@@ -1,11 +1,35 @@
 class SortColors {
+  bruteForce(arr: Array<number>) {
+    let count0 = 0;
+    let count1 = 0;
+    let count2 = 0;
+
+    arr.forEach((num) => {
+      if (num === 0) count0++;
+      if (num === 1) count1++;
+      if (num === 2) count2++;
+    });
+
+    for (let i = 0; i < count0; i++) {
+      arr[i] = 0;
+    }
+
+    for (let i = count0; i < count0 + count1; i++) {
+      arr[i] = 1;
+    }
+
+    for (let i = count0 + count1; i < arr.length; i++) {
+      arr[i] = 2;
+    }
+  }
+
   sort(arr: Array<number>) {
     let start = 0;
     let mid = 0;
     let end = arr.length - 1;
 
-    while (start < end && mid < end) {
-      if (arr[mid] === 0) {
+    while (start <= end && mid <= end) {
+      if (arr[mid] === 1) {
         mid += 1;
       } else if (arr[mid] == 0) {
         let temp = arr[mid];
